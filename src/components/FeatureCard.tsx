b47ddece06b0ac2fc2f6cf4ef37c37edc3b9212f
jahan siddiqui <raj918833@gmail.com>
2024-12-13 23:21:56 +0530

    feature section responsive done with some minor changes

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,11 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
 export default function FeatureCard(props: {
     title: string;
     description: string;
     children?: React.ReactNode;
+    className?: string;
 }) {
-    const { title, description, children } = props;
+    const { title, description, children, className } = props;
     return (
-        <div className=" bg-neutral-900 border border-white/10 rounded-3xl p-6">
+        <div className={twMerge("bg-neutral-900 border border-white/10 rounded-3xl p-6", className)} >
             <div className=" aspect-video">{children}</div>
             <div>
                 <h3 className="text-3xl font-medium mt-6">{title}</h3>
