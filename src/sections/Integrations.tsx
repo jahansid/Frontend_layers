a8c807b79de44bcac02206d1ee3cce3d67c11ef6
jahan siddiqui <raj918833@gmail.com>
2024-12-16 00:38:28 +0530

    Integrations section done with responsive

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,39 +1,5 @@
 import IntegrationColumn from "@/components/IntegrationColumn";
 import Tag from "@/components/Tag";
-import Image from "next/image";
-
-// const integrations = [
-//     {
-//         name: "Figma",
-//         icon: figmaIcon,
-//         description: "Figma is a collaborative interface design tool.",
-//     },
-//     {
-//         name: "Notion",
-//         icon: notionIcon,
-//         description: "Notion is an all-in-one workspace for notes and docs.",
-//     },
-//     {
-//         name: "Slack",
-//         icon: slackIcon,
-//         description: "Slack is a powerful team communication platform.",
-//     },
-//     {
-//         name: "Relume",
-//         icon: relumeIcon,
-//         description: "Relume is a no-code website builder and design system.",
-//     },
-//     {
-//         name: "Framer",
-//         icon: framerIcon,
-//         description: "Framer is a professional website prototyping tool.",
-//     },
-//     {
-//         name: "GitHub",
-//         icon: githubIcon,
-//         description: "GitHub is the leading platform for code collaboration.",
-//     },
-// ];
 
 const integrations = [
     {
@@ -74,20 +40,30 @@ export default function Integrations() {
     return (
         <section className=" py-24">
             <div className=" container">
-                <div className=" flex justify-center items-center">
+                <div className=" grid lg:grid-cols-2 items-center lg:gap-16">
+                    <div>
+                
                     <Tag>Integrations</Tag>
-                </div>
-                <h2 className=" text-6xl font-medium mt-6 text-center">
+               
+                <h2 className=" text-6xl font-medium mt-6 ">
                     Plays well with{" "}
                     <span className="text-lime-400">others</span>
                 </h2>
-                <p className=" text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                <p className=" text-xl text-white/50 mt-8 max-w-2xl ">
                     Layers seamlessly connects with your favorite tools, making
                     it easy to plug into any workflow and collaborate across
                     platforms.
                 </p>
-                <div className=" h-[400px] mt-8 overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
-                    <IntegrationColumn integrations={integrations}/>
+                </div>
+                <div>
+                <div className=" h-[400px] lg:h-[800px] mt-8 lg:mt-0 grid md:grid-cols-2 gap-4 overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+                    <IntegrationColumn integrations={integrations} />
+                    <IntegrationColumn
+                        integrations={integrations.slice().reverse()}
+                        className=" hidden md:flex"
+                    />
+                </div>
+                </div>
                 </div>
             </div>
         </section>
