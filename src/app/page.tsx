fbde97e2fb9a3e44e2e6e15c360dda3611f6ce25
jahan siddiqui <raj918833@gmail.com>
2024-12-16 16:25:24 +0530

    FAQ section added with responsive approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Hero from "@/sections/Hero";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
+import Faqs from "@/sections/Faqs";
 
 export default function Home() {
     return (
@@ -16,6 +17,7 @@ export default function Home() {
             <Introduction />
             <Features />
             <Integrations />
+            <Faqs />
         </>
     );
 }
