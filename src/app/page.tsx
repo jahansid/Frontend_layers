05d2efff50c5f52fe81717b07fa71ba8259cb9e5
jahan siddiqui <raj918833@gmail.com>
2024-12-16 19:06:54 +0530

    callToaction section added with responsive approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
 import Faqs from "@/sections/Faqs";
+import CallToAction from "@/sections/CallToAction";
 
 export default function Home() {
     return (
@@ -18,6 +19,7 @@ export default function Home() {
             <Features />
             <Integrations />
             <Faqs />
+            <CallToAction />
         </>
     );
 }
