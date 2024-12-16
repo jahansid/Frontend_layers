4cb492db8d5830886d751e505601b8c73dc790f9
jahan siddiqui <raj918833@gmail.com>
2024-12-16 20:15:08 +0530

    LogoTicker section added with responsive approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
-// import LogoTicker from "@/sections/LogoTicker";
+import LogoTicker from "@/sections/LogoTicker";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
@@ -15,7 +15,7 @@ export default function Home() {
         <>
             <Navbar />
             <Hero />
-            {/* <LogoTicker /> */}
+            <LogoTicker />
             <Introduction />
             <Features />
             <Integrations />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,32 +1,35 @@
-import quantumLogo from "@/assets/images/quantum.svg";
-import acmeLogo from "@/assets/images/acme-corp.svg";
-import echoValleyLogo from "@/assets/images/echo-valley.svg";
-import pulseLogo from "@/assets/images/pulse.svg";
-import outsideLogo from "@/assets/images/outside.svg";
-import apexLogo from "@/assets/images/apex.svg";
-import celestialLogo from "@/assets/images/celestial.svg";
-import twiceLogo from "@/assets/images/twice.svg";
+import Image from "next/image";
 
 const logosData = [
-    { name: "Quantum", logoImage: quantumLogo },
-    { name: "Acme Corp", logoImage: acmeLogo },
-    { name: "Echo Valley", logoImage: echoValleyLogo },
-    { name: "Pulse", logoImage: pulseLogo },
-    { name: "Outside", logoImage: outsideLogo },
-    { name: "Apex", logoImage: apexLogo },
-    { name: "Celestial", logoImage: celestialLogo },
-    { name: "Twice", logoImage: twiceLogo },
+    { name: "Quantum", logoImage: "/assets/icons/quantum.svg" },
+    { name: "Acme Corp", logoImage: "/assets/icons/acme-corp.svg" },
+    { name: "Echo Valley", logoImage: "/assets/icons/echo-valley.svg" },
+    { name: "Pulse", logoImage: "/assets/icons/pulse.svg" },
+    { name: "Outside", logoImage: "/assets/icons/outside.svg" },
+    { name: "Apex", logoImage: "/assets/icons/apex.svg" },
+    { name: "Celestial", logoImage: "/assets/icons/celestial.svg" },
+    { name: "Twice", logoImage: "/assets/icons/twice.svg" },
 ];
 
 const LogoTicker = () => {
     return (
         <section className=" py-24 overflow-x-clip">
             <div className="container">
-                <h3>Already chosen by these market leaders.</h3>
-                <div>
-                <div>  
-                       
-                    </div>  
+                <h3 className=" text-xl text-center text-white/50">
+                    Already chosen by these market leaders.
+                </h3>
+                <div className=" overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] ">
+                    <div className=" flex gap-24 pr-24">
+                        {logosData.map((logo) => (
+                            <Image
+                                src={logo.logoImage}
+                                width={150}
+                                height={50}
+                                alt={logo.name}
+                                key={logo.name}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
